980b5cad6fabdee7a2e9c0e54351808e1843067a
Richard Valdez <45345760+repocdlf@users.noreply.github.com>
2020-09-26 18:44:32 -0300

    Pantallas y navegacion

---
+++ app/src/main/java/ar/edu/ort/instituto/echeff/fragments/cliente/VistaReservasFragment.kt
@@ -0,0 +1,27 @@
+package ar.edu.ort.instituto.echeff.fragments.cliente
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ar.edu.ort.instituto.echeff.R
+
+
+class VistaReservasFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vista_reservas, container, false)
+    }
+
+}
