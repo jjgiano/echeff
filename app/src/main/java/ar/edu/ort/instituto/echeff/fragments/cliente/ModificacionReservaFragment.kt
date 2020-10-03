d0169636a9b48123e98eb20baef0d3d48f748470
acenturion <alejandrocenturion@quiena.com>
2020-10-03 18:13:43 -0300

    Agrada conexion con kotlin

--- app/src/main/java/ar/edu/ort/instituto/echeff/fragments/cliente/ModificacionReservaFragment.kt
+++ app/src/main/java/ar/edu/ort/instituto/echeff/fragments/cliente/ModificacionReservaFragment.kt
@@ -5,20 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import ar.edu.ort.instituto.echeff.R
+import com.google.android.material.snackbar.Snackbar
 
 
 class ModificacionReservaFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var v: View
+    private lateinit var btnEnviarModificaion: Button
+    private lateinit var txtCambiosReserva: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modificacion_reserva, container, false)
+        v = inflater.inflate(R.layout.fragment_modificacion_reserva, container, false)
+
+        btnEnviarModificaion =v.findViewById(R.id.btnEnviarCambios)
+        txtCambiosReserva = v.findViewById(R.id.txtCambiosReserva)
+
+        return v
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        //TODO
+        btnEnviarModificaion.setOnClickListener(){
+            Snackbar.make(v, "Se envio la modificacion al chef", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
