8b00bfbd130f2de16de75fc767b94c672482e295
Juan.Gianotti <jjgiano@gmail.com>
2020-10-29 22:06:41 -0300

    Estado de Usario cambiado a Entero

--- app/src/main/java/ar/edu/ort/instituto/echeff/entities/Chef.kt
+++ app/src/main/java/ar/edu/ort/instituto/echeff/entities/Chef.kt
@@ -2,7 +2,7 @@ package ar.edu.ort.instituto.echeff.entities
 
 class Chef(
     id: String, nombre: String, email: String, urlFoto: String,
-    estado: String, telefono: String
+    estado: Int, telefono: String
 ) : Usuario(
     id, nombre, email,
     urlFoto, estado, telefono
--- app/src/main/java/ar/edu/ort/instituto/echeff/fragments/RegistroUsuarioFragment.kt
+++ app/src/main/java/ar/edu/ort/instituto/echeff/fragments/RegistroUsuarioFragment.kt
@@ -17,6 +17,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import ar.edu.ort.instituto.echeff.R
 import ar.edu.ort.instituto.echeff.entities.Cliente
+import ar.edu.ort.instituto.echeff.entities.EstadoUsuario
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
@@ -119,7 +120,7 @@ class RegistroUsuarioFragment : Fragment() {
                         nombre.text.toString(),
                         userEmail,
                         profilePhotoRef,
-                        "",
+                        EstadoUsuario.ACTIVO.id,
                         telefono.text.toString()
                     )
                 )
@@ -130,7 +131,7 @@ class RegistroUsuarioFragment : Fragment() {
                         userDisplayName,
                         userEmail,
                         profilePhotoRef,
-                        "",
+                        EstadoUsuario.ACTIVO.id,
                         telefono.text.toString()
                     )
                 )
